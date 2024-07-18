b59c42f3f0fea6d707af68932064c7ec0f8c5585
Ömer Bıçakcıoğlu <omer.bicakcioglu@ug.bilkent.edu.tr>
2024-07-18 16:48:59 +0300

    deneme

--- constants/clotche_specifications.py
+++ constants/clotche_specifications.py
@@ -1,13 +1,11 @@
 valid_sizes = ["Oversize", "Slim", "Regular"]
 valid_types = ["T-shirt",
-           "Pant",
-           "Jacket",
-           "Shirt",
-           "Shoe",
-           "Sweatshirt",
-           "Short",
-           "Socks",
-           ]
+               "Pant",
+               "Jacket",
+               "Shirt",
+               "Shoe",
+               "Sweatshirt",
+               "Short",
+               "Socks",
+               ]
 valid_sexes = ["Male", "Female", "Unisex"]
-
-
